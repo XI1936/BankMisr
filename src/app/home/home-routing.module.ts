ce061d675e8ac8d7402cc3ecba105185ac8fee03
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2021-03-19 15:39:14 +0530

    two new comp. added

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,8 +1,10 @@
+import { AccountsComponent } from './accounts/accounts.component';
 import { AllbudgetsComponent } from './allbudgets/allbudgets.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { CommonModule } from '@angular/common';
+import { AddAccountComponent } from './add-account/add-account.component';
 const homeRoutes: Routes = [
   {
     path: 'home',
@@ -11,6 +13,14 @@ const homeRoutes: Routes = [
       {
         path: 'allbudgets',
         component: AllbudgetsComponent
+      },
+      {
+        path: 'accounts',
+        component: AccountsComponent
+      },
+      {
+        path: 'addaccount',
+        component: AddAccountComponent
       },
       { path: '', redirectTo: 'home', pathMatch: 'full' }
     ]
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { HomeRoutingModule } from './home-routing.module';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AccountsComponent } from './accounts/accounts.component';
+import { AddAccountComponent } from './add-account/add-account.component';
 
 @NgModule({
-  declarations: [AllbudgetsComponent],
+  declarations: [AllbudgetsComponent, AccountsComponent, AddAccountComponent],
   imports: [HomeRoutingModule,  CommonModule, FormsModule, ReactiveFormsModule],
   providers: []
 })
