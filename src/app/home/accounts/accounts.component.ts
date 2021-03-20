ca78b1778d3fe5a2b422a29bd8899c85b0d6946e
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2021-03-20 10:01:27 +0530

    implemented story 2 for accounts details sorted with balance

--- src/app/home/accounts/accounts.component.ts
+++ src/app/home/accounts/accounts.component.ts
@@ -1,3 +1,4 @@
+import { AppService } from './../../services/app.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountsComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private appSevice:AppService) { }
+  budgetNameArr:any=[];
+  accountArr:any=[];
+  isLoaded:boolean = false;
   ngOnInit(): void {
+    this.appSevice.getAllbudgets().subscribe((data=>{
+      let budget=data['data'].budgets;
+      budget.forEach(element => {
+        this.budgetNameArr.push({key:element.id,value:element.name})
+      });
+    }));
+  }
+
+  selectedBudget(value){
+    this.isLoaded=true;
+    this.appSevice.getBudgetAccounts(value).subscribe((data)=>{
+      this.accountArr=data['data'].accounts;
+      this.accountArr.sort((a,b)=>{
+        return b.balance - a.balance ;
+      })
+      this.isLoaded=false;
+    })
+    
   }
 
 }
