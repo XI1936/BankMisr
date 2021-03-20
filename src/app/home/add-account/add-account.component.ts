e8f9e3fe6223d22d4c06b19a219f32638bc09691
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2021-03-20 21:13:20 +0530

    story 3 completed for add account

--- src/app/home/add-account/add-account.component.ts
+++ src/app/home/add-account/add-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from 'src/app/services/app.service';
 
 @Component({
   selector: 'app-add-account',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddAccountComponent implements OnInit {
   accTypeArr=[ "checking", "savings", "creditCard", "cash", "lineOfCredit", "otherAsset", "otherLiability" ];
-  constructor() { }
-
+  budgetNameArr:any=[];
+  isLoaded:boolean=false;
+  isSaved:boolean=false;
+  constructor(private appSevice:AppService) { }
+   
   ngOnInit(): void {
+    this.appSevice.getAllbudgets().subscribe((data=>{
+      let budget=data['data'].budgets;
+      budget.forEach(element => {
+        this.budgetNameArr.push({key:element.id,value:element.name})
+      });
+    }));
   }
 
+  onSubmit(data){
+    this.isLoaded=true
+    let budgetId=data.budgetId;
+    let newAccData={
+      "account":{
+        "name": data.accName,
+      "type": data.accType,
+      "balance": data.accBalance
+      }
+    }
+    this.appSevice.createAccount(newAccData,budgetId).subscribe((resp=>{
+      this.isSaved=true;
+      this.isLoaded=false;
+    })); 
+  }
+
+  closePop(){
+    this.isSaved=false;
+  }
 }
